070b969f11b2f0acf62d90fad32bc337c768d3e6
R-MEDA <101333735+R-MEDA@users.noreply.github.com>
2022-04-26 11:50:44 +0200

    test written for divide computation

--- 2021Rekenmachine/src/test/java/JavaFXAppTest.java
+++ 2021Rekenmachine/src/test/java/JavaFXAppTest.java
@@ -2,6 +2,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaFXAppTest {
+
+
+    @Test
+    void divisionTest()
+    {
+        JavaFXApp j = new JavaFXApp();
+        int expected = 10;
+        int result = j.computeDivide(20,2);
+        assertEquals(expected,result);
+    }
+
+
     @Test
     void aDummyTest() {
         assertEquals(0,0);
