8bcff5bd6281bd05decbbb53d435df9799c757a7
treinDotJS <82215214+treinDotJS@users.noreply.github.com>
2022-04-26 11:52:52 +0200

    add computeMultiplyTest

--- 2021Rekenmachine/src/test/java/JavaFXAppTest.java
+++ 2021Rekenmachine/src/test/java/JavaFXAppTest.java
@@ -1,9 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaFXAppTest {
+    private JavaFXApp javaFXApp;
+    @BeforeEach
+    void setUp() {
+        javaFXApp = new JavaFXApp();
+    }
+
     @Test
     void aDummyTest() {
         assertEquals(0,0);
+    }
+
+    @Test
+    void computeMultiplyTest() {
+        //Arrange
+        int expected = 15;
+
+        //Act
+        int actual = javaFXApp.computeMultiply(5,3);
+
+        //Assert
+        Assertions.assertEquals(expected, actual);
     }
 }
